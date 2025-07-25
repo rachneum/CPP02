12cf892bee025fb48ba95f6738a0441fc4309703
Klaenex <klaenex.29@gmail.com>
2025-07-25 18:27:36 +0200

    test

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -8,16 +8,16 @@ Fixed::Fixed() : _value(0)
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
-    *this = other; // on appelle l'opérateur =
+    //this est un pointeur automatique que cpp me donne toujours dans toutes mes fonnctions membres non statiques.
+    *this = other;//J'appelle l'opérateur =.
 }
 
 Fixed &Fixed::operator=(const Fixed &rhs)
 {
     std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &rhs) {
-        this->_value = rhs.getRawBits();
-    }
-    return *this;
+    if (this != &rhs)
+        _value = rhs.getRawBits();
+    return (*this);
 }
 
 Fixed::~Fixed()
