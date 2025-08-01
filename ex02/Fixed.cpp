093966ac333820d01eb43503cfbaa8272b72d60a
Klaenex <klaenex.29@gmail.com>
2025-08-01 23:34:00 +0200

    test

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -23,7 +23,7 @@ Fixed::Fixed(const Fixed &other)
     *this = other;
 }
 
-Fixed& Fixed::operator=(const Fixed& rhs)//Opérateur d’affectation.
+Fixed& Fixed::operator=(const Fixed& rhs)
 {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &rhs)
@@ -56,7 +56,7 @@ int Fixed::toInt() const
     return (_value >> _fractionalBits);
 }
 
-bool Fixed::operator>(const Fixed& rhs) const//Comparaison.
+bool Fixed::operator>(const Fixed& rhs) const
 {
     return (_value > rhs._value);
 }
@@ -86,17 +86,17 @@ bool Fixed::operator!=(const Fixed& rhs) const
     return (_value != rhs._value);
 }
 
-Fixed Fixed::operator+(const Fixed& rhs) const//Arithmétique.
+Fixed Fixed::operator+(const Fixed& rhs) const
 {
     Fixed res;
-    res.setRawBits(this->_value + rhs._value);
+    res.setRawBits(_value + rhs._value);
     return (res);
 }
 
 Fixed Fixed::operator-(const Fixed& rhs) const
 {
     Fixed res;
-    res.setRawBits(this->_value - rhs._value);
+    res.setRawBits(_value - rhs._value);
     return (res);
 }
 
@@ -104,7 +104,7 @@ Fixed Fixed::operator*(const Fixed& rhs) const
 {
     Fixed res;
     long result = (long)_value * (long)rhs._value;
-    res.setRawBits(result >> _fractionalBits);
+    res.setRawBits(result >> _fractionalBits);//>>_fB pour revenir à la bonne échelle de nombre à virgule fixe, après avoir multiplié mes deux valeurs entières.
     return (res);
 }
 
@@ -116,12 +116,12 @@ Fixed Fixed::operator/(const Fixed& rhs) const
         return (Fixed());
     }
     Fixed res;
-    long result = ((long)_value << _fractionalBits) / rhs._value;
+    long result = ((long)_value << _fractionalBits) / rhs._value;//On multiplie seulement _value par 256 pour augmenter la précision du numérateur avant division entière.
     res.setRawBits(result);
     return (res);
 }
 
-Fixed& Fixed::operator++()//Incrémentation/Décrémentation.
+Fixed& Fixed::operator++()
 {
     _value += 1;
     return (*this);
@@ -147,13 +147,13 @@ Fixed Fixed::operator--(int)
     return (tmp);
 }
 
-Fixed& Fixed::min(Fixed& a, Fixed& b)//Fonctions statiques.
+Fixed& Fixed::min(Fixed& a, Fixed& b)//Référence non const, je peux modifier a et b.
 {
     return ((a < b) ? a : b);
 }
 
-const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
-{
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b)//Compare mes 2 objets et renvoie une référence au plus petit.
+{//Si a strrictement plus petit que b, retourne a. Sinon retourne b.
     return ((a < b) ? a : b);
 }
 
